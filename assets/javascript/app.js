4cb40c1a7bcbe3b1f837f7a7d86516f9c3f8169b
thayes3116 <timhayes3116@gmail.com>
2017-06-04 15:11:10 -0400

    needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,27 @@
-var topics = ["Thing", "Silver Surfer", "Captain America", "Thor", "Spiderman", "Batman", "Superman", "Wonder Woman", "Robin", "Green Lantern"];
+var topics = ["Silver Surfer", "Captain America", "Thor", "Spiderman", "Batman", "Superman", "Wonder Woman", "Robin", "Green Lantern"];
 
+var animate = 0;
 //get the document ready
 $(document).ready(function() {
     console.log("ready!");
 
+    createButton();
+
     $("#addhero").on("click", function(event) {
         event.preventDefault();
 
+        //Capture user input
         var hero = $("#newhero").val().trim()
-            .toLowerCase();
-        topics.push(hero);
-        createButton();
-
+            //prevent creation of empty buttons
+            if(hero !== ""){
+                //Add user input to array and fire function to make new button
+                topics.push(hero);
+                createButton();
+            }
     });
-
+    //function to make new button
     function createButton() {
-
+        //empty button div to prevent repeat buttons
         $("#buttons").empty();
 
         for (var i = 0; i < topics.length; i++) {
@@ -27,8 +33,12 @@ $(document).ready(function() {
         }
     }
 
+
+
+
+// }    		
     function displayGif() {
-    	$(".heorgifs").empty();
+    	$(".herogifs").empty();
         var apikey = "dc6zaTOxFJmzC";
         var typedhero = $(this).attr("data-name");
         var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + typedhero + "&limit=10&api_key=" + apikey;
@@ -42,19 +52,43 @@ $(document).ready(function() {
             for (var i = 0; i < 10; i++) {
                 var herodiv = $("<div class = hero>");
                 var rating = response.data[i].rating;
-                console.log(response.data[i]);
-
                 var prating = $("<p class = rating>").text("Rating: " + rating);
                 herodiv.prepend(prating);
-                var herogifsrc = response.data[i].images.fixed_height_still.url;
-                var herogif = $("<img>").attr("src", herogifsrc);
-                herodiv.prepend(herogif);
+                var herogifsrcstill = response.data[i].images.fixed_width_still.url;
+                //var herogifsrc = response.data[i].images.fixed_width.url;
+                var herogifstill = $("<img class = heroimg>").attr("src", herogifsrcstill).val(i);
+                    
+                herodiv.prepend(herogifstill);
                 $(".herogifs").append(herodiv);
-                
-            }
+}
+            
+            $(".heroimg").on("click", function(event) {
+                    event.preventDefault();
+                    
+                if (animate == 0){ 
+                    //console.log("heroimg click");
+                    //console.log(this.value)
+                    var imgvalue = this.value;
+                    //console.log(imgvalue)
+                    $(this).attr("src", response.data[imgvalue].images.fixed_width.url);
+                    animate++
+                    console.log(animate);
+                }else{
+                    var imgvalue = this.value;
+                    $(this).attr("src", response.data[imgvalue].images.fixed_width_still.url);
+                    animate--
+                    console.log(animate);
+                }
+                });
+   
+            
         });
     };
 
     $(document).on("click", ".herobtn", displayGif);
-    	createButton();
+
+   
+
+
+   				
 });
